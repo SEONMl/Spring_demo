81c7c924783299c64b63c84196a748c3a9472c0e
sm000117 <75351686+ParkSeonMi@users.noreply.github.com>
2022-01-16 17:32:35 +0900

    10강

--- src/main/java/com/example/demo/Controller/ArticleController.java
+++ src/main/java/com/example/demo/Controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.demo.Controller;
 import com.example.demo.dto.ArticleForm;
 import com.example.demo.entity.Article;
 import com.example.demo.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j // 로깅을 위한 골뱅이(annotation)
 public class ArticleController {
 
     @Autowired  //스프링 부트가 미리 생성해 놓은 객체를 가져다가 자동 연결!
@@ -21,14 +23,18 @@ public class ArticleController {
 
     @PostMapping("/articles/create") //form이 던진 데이터를 받는다. 객체(DTO)로 받는다
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
+//        System.out.println(form.toString()); --> 로깅기능으로 대체!
 
         //1. Dto를 Entity로 변환 | id=null
         Article article = form.toEntity();
+        log.info(article.toString());
+//        System.out.println(article.toString());
 
         //2. Repository에게 Entity를 DB안에 저장하게 함 | id 자동 할당
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+//        System.out.println(saved.toString());
 
         return "";
     }
--- src/main/java/com/example/demo/entity/Article.java
+++ src/main/java/com/example/demo/entity/Article.java
@@ -1,12 +1,18 @@
 package com.example.demo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@AllArgsConstructor //모든 생성자 사용가능
+@ToString //롬복으로 코드 리팩토링
 @Entity //DB가 해당 객체를 인식 가능
 public class Article {
+
     @Id //대표값을 지정 like a 주민등록번호
     @GeneratedValue //1, 2, 3, ... 자동 생성 어노테이션
     private Long id;
@@ -16,19 +22,4 @@ public class Article {
 
     @Column
     private String content;
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }

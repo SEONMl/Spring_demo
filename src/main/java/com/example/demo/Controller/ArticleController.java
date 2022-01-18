05401dd6148ff63c48ed01175c860cb5e713511e
sm000117 <75351686+ParkSeonMi@users.noreply.github.com>
2022-01-18 23:28:29 +0900

    13강

--- src/main/java/com/example/demo/Controller/ArticleController.java
+++ src/main/java/com/example/demo/Controller/ArticleController.java
@@ -6,8 +6,13 @@ import com.example.demo.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @Slf4j // 로깅을 위한 골뱅이(annotation)
@@ -17,7 +22,7 @@ public class ArticleController {
     private ArticleRepository articleRepository; //객체를 만들지 않아도 스프링부트가 자동적으로 생성
 
     @GetMapping("/articles/new")
-    public String newArticleForm(){
+    public String newArticleForm() {
         return "articles/new";
     }
 
@@ -36,6 +41,32 @@ public class ArticleController {
         log.info(saved.toString());
 //        System.out.println(saved.toString());
 
-        return "";
+        return "redirect:/articles/" + saved.getId() ;
+    }
+
+    @GetMapping("/articles/{id}") //id에 들어가는 건 변수다.
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = "+ id);
+        // 1. id로 데이터를 가져옴  (데이터를 가져오는 주체=repository | Autowired를 통해 가져온 articleRepository)
+        // Optional<Article> articleEntity = articleRepository.findById(id); ---> 이 코드는 지양
+        Article articleEntity = articleRepository.findById(id).orElse(null); // id에 해당하는 값이 없다면 null을 반환
+
+        // 2. 가져온 데이터를 모델에 등록
+        model.addAttribute("article", articleEntity);
+
+        // 3. 보여줄 페이지를 설정
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1. 모든 Article을 가져온다
+        List<Article> articleEntityList = articleRepository.findAll(); //데이터 전체 조회
+
+        // 2. 가져온 Article 묶음을 뷰로 전달
+        model.addAttribute("articleList", articleEntityList);
+
+        // 3. 뷰 페이지를 설정
+        return "articles/index"; //articles/index.mustache
     }
 }
--- src/main/java/com/example/demo/entity/Article.java
+++ src/main/java/com/example/demo/entity/Article.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -9,8 +11,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @AllArgsConstructor //모든 생성자 사용가능
+@NoArgsConstructor //디폴트 생성자를 추가하는 롬복
 @ToString //롬복으로 코드 리팩토링
 @Entity //DB가 해당 객체를 인식 가능
+@Getter //모든 게터 추가
 public class Article {
 
     @Id //대표값을 지정 like a 주민등록번호
@@ -22,4 +26,12 @@ public class Article {
 
     @Column
     private String content;
+
+    /*
+    게터를 만들어도 되지만 우리는? 롬복을 쓴다.
+    public Long getId() {
+        return this.id;
+    }
+
+     */
 }
